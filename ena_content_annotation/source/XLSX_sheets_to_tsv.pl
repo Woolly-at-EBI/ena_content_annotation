cebe008d6867cdd98815b55070cbd223eadf90a2
woollard <woollard@ebi.ac.uk>
2024-10-31 15:17:59 +0000

    initial: files and docs

---
+++ ena_content_annotation/source/XLSX_sheets_to_tsv.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Script of XLSX_sheets_to_tsv.pl is to convert all sheets in an XLSX file to tsv
+
+___author___ = "woollard@ebi.ac.uk"
+___start_date___ = 2024-10-31
+__docformat___ = 'reStructuredText'
+
+"""
+
+
+import logging
+logger = logging.getLogger(__name__)
+import argparse
+
+def first_func():
+    """"""
+
+    print()
+
+def main():
+    first_func()
+
+if __name__ == '__main__':
+    logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s - %(message)s')
+    main()
