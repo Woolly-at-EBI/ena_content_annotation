01056e4ffcb79d361d676f6c18f0dc703531a56c
woollard <woollard@ebi.ac.uk>
2024-10-31 15:19:03 +0000

    implemented: working XLSX_sheets_to_tsv.pl

--- ena_content_annotation/source/XLSX_sheets_to_tsv.pl
+++ ena_content_annotation/source/XLSX_sheets_to_tsv.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Script of XLSX_sheets_to_tsv.pl is to convert all sheets in an XLSX file to tsv
+"""Script of XLSX_sheets_to_tsv.pl is to convert  sheets of XLSX to tsv
 
 ___author___ = "woollard@ebi.ac.uk"
 ___start_date___ = 2024-10-31
@@ -11,15 +11,55 @@ __docformat___ = 'reStructuredText'
 import logging
 logger = logging.getLogger(__name__)
 import argparse
+import pandas as pd
+import os
 
-def first_func():
+def read_excel(infile):
     """"""
+    logger.debug(f"infile={infile}")
+    try:
+        #df = pd.read_excel(infile)
+        sheets_dict = pd.read_excel(infile, sheet_name=None)
+        #sheet_keys = df.keys()
+        #print(sheet_keys)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
-    print()
+    # save each DataFrame to a variable dynamically if needed
+    sheet_dataframes = {sheet_name: df for sheet_name, df in sheets_dict.items()}
+    logger.debug(sheet_dataframes.keys())
+
+    directories = os.path.dirname(infile)
+
+    for sheet in sheet_dataframes:
+        logger.info(sheet)
+        df = sheet_dataframes[sheet]
+        logger.debug(df.head())
+        outfile_name = directories + "/" + sheet.replace(" ", "_") + ".xlsx"
+        logger.debug(f"{outfile_name}")
+        df.to_csv(outfile_name, sep="\t", index=False)
+        logger.info(f"created: {outfile_name}")
+
+
+
+
+
 
 def main():
-    first_func()
+    read_excel(args.file)
 
 if __name__ == '__main__':
     logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s - %(message)s')
-    main()
+    parser = argparse.ArgumentParser(
+                    prog='XLSX_sheets_to_tsv.pl',
+                    description='convert  sheets of XLSX to tsv',
+                    epilog='')
+    parser.add_argument("-f", "--file", help="XLSX file to convert")
+    args = parser.parse_args()
+    if args.file:
+        logger.debug(f"args.file selected value={args.file}")
+        main()
+    else:
+        parser.print_help()
+
+
